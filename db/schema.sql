cbba54f3810ba892cdd386c6112d8806474ff716
Betty Lu <bettylu@Bettys-MacBook-Pro.local>
2023-02-02 23:10:56 -0800

    modified schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -11,9 +11,9 @@ CREATE TABLE department (
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL,
-  department_id INT NOT NULL,
-  FOREIGN KEY (department)
+  salary DECIMAL NOT NULL,
+  department_id INT,
+  FOREIGN KEY (department_id)
   REFERENCES department(id)
   ON DELETE SET NULL
 );
@@ -22,10 +22,13 @@ CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT NULL,
+    role_id INT,
+    manager_id INT,
     FOREIGN KEY (role_id)
     REFERENCES role(id)
+    ON DELETE SET NULL,
+    FOREIGN KEY (id)
+    REFERENCES (manager_id)
     ON DELETE SET NULL
 )
 
